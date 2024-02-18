06da470318d626feb310972e38c07bf4f463987e
Kenia Crisoni <keniafcrisoni@gmail.com>
2024-02-18 16:42:40 -0300

    adicionando SmartTv

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,23 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.diminuirVolume();
+        System.out.println("Qual o volume atual?  "); 
+
+        smartTv.mudarCanal();
+        System.out.println("Qual o canal atual?  "); 
+
+        System.out.println(" TV Ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("volume atual: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status - TV ligada? " + smartTv.ligada );
+        smartTv.desligar();
+        System.out.println("Novo Status - TV desligada? " + smartTv.ligada );
+        
+    }
+}
---
+++ tipos-variaveis/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+   public static void main(String[] args) {
+    int a, b;
+    a = 5;
+    b = 6;
+    String resultado = a==b ? "verdadeiro" : "falso";
+    //   if (a==b)
+    //   resultado = "verdadeiro";
+    //   else 
+    //   resultado = "verdadeiro";
+      
+    System.out.println(resultado);
+   
+    
+   }
+}
